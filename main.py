32619d64fce76f8218fa08170f02effc7a4cc6c4
Aman <manti1102@icloud.com>
2024-05-11 19:56:25 +0600

    написали логику к,н,б

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import telebot
 from local_settings import API_TOKEN
+import random
+
 
 
 bot = telebot.TeleBot(API_TOKEN)
@@ -8,16 +10,29 @@ bot = telebot.TeleBot(API_TOKEN)
 
 @bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
-    bot.reply_to(message, """\
-Hi there, I am EchoBot.
-I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
-""")
+    bot.reply_to(message, "Добра пожаловать в игру ' камень ножницы бумага' !"
+                          "Выберите камень (к), ножницы (н) или бумагу (б)")
 
 
 
 @bot.message_handler(func=lambda message: True)
-def echo_message(message):
-    bot.reply_to(message, message.text)
+def play_game(message):
+    user_move = message.text
+    if user_move not in ["к", "н", "б"]:
+        bot.reply_to(message, "Пожалуйста введите либо к, н или б")
+    else:
+        computer_move = random.choice(["к", "н", "б"])
+        if (user_move == "к" and computer_move == "н") or\
+                (user_move == "н" and computer_move == "б") or \
+                (user_move == "б" and computer_move == "к"):
+            bot.reply_to(message, "Вы выиграли!")
+        elif user_move == computer_move:
+            bot.reply_to(message, "Ничья!")
+        else:
+            bot.reply_to(message, "Бот выиграл!")
+
+
+
 
 
 bot.infinity_polling()
